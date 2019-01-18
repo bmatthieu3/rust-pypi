c06822720b441d0c7c8710c59f600d3ea311dbc9
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2019-01-18 10:13:28 +0100

    v1.8.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="rust_pypi",
-    version="1.7.0",
+    version="1.8.0",
     rust_extensions=[RustExtension("rust_pypi.rust_pypi", 'Cargo.toml', binding=Binding.NoBinding)],
     packages=["rust_pypi"],
     install_requires=[
